1034a2d73b687dfe72f77c5182a4c574d638f337
Jibeen Ryu <logan.ryu@gmail.com>
2025-11-20 03:31:58 +0900

    정답 오답 사운드 추가

---
+++ app/src/main/java/com/example/quizapp/SoundPlayer.kt
@@ -0,0 +1,27 @@
+package com.example.quizapp
+
+import android.content.Context
+import android.media.MediaPlayer
+import androidx.annotation.RawRes
+
+class SoundPlayer(private val context: Context) {
+    private var mediaPlayer: MediaPlayer? = null
+
+    fun playSound(@RawRes soundResId: Int) {
+        mediaPlayer?.release()
+        mediaPlayer = null
+
+        mediaPlayer = MediaPlayer.create(context, soundResId)
+        mediaPlayer?.start()
+
+        mediaPlayer?.setOnCompletionListener { mp ->
+            mp.release()
+            mediaPlayer = null
+        }
+    }
+
+    fun release() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+}
